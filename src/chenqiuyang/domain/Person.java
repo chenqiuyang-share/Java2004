0efa6e45cf98659052ac79cea253e0d108533ff1
chenqiuyang-share <1182492999@qq.com>
2020-09-20 03:33:36 +0800

    添加了phone属性

--- src/chenqiuyang/domain/Person.java
+++ src/chenqiuyang/domain/Person.java
@@ -6,6 +6,7 @@ public class Person {
 
     private Integer age;
     private String password;
+    private String phone;
     public Person(String username, Integer age) {
         this.username = username;
         this.age = age;
